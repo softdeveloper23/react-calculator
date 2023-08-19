da450de3ab2fc28572f298a1ef466e54fb3caaaa
unknown <brannongarrett77@gmail.com>
2023-08-19 10:37:46 +0200

    Added the 'evaluate' function

--- src/App.js
+++ src/App.js
@@ -39,9 +39,38 @@ function reducer(state, { type, payload }) {
         }
       }
 
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null
+      }
+
     case ACTIONS.CLEAR:
       return {}
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+  switch (operation) {
+    case "+":
+      computation = prev + current
+      break
+    case "-":
+      computation = prev - current
+      break
+    case "*":
+      computation = prev * current
+      break
+    case "/":
+      computation = prev / current
+      break
+  }
+  return computation.toString()
 }
 
 function App() {
