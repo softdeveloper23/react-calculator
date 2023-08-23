1c47dd3aa1cda80f4352d09dd006871f8242cf71
unknown <brannongarrett77@gmail.com>
2023-08-23 18:36:09 +0200

    DEL button now functions

--- src/App.js
+++ src/App.js
@@ -139,7 +139,7 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="/" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
