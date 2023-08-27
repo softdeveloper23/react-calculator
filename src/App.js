9b5731593cae08e42370dd92d47ff1a2cf387bea
unknown <brannongarrett77@gmail.com>
2023-08-27 19:01:12 +0200

    App is complete

--- src/App.js
+++ src/App.js
@@ -131,6 +131,8 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-US", {
 function formatOperand(operand) {
   if (operand == null) return
   const [integer, decimal] = operand.split(".")
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
 function App() {
@@ -139,8 +141,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{previousOperand}{operation}</div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="previous-operand">{formatOperand(previousOperand)}{operation}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button
         className="span-two"
